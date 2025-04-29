d6621eabaf27cfd0e44382e04c914388834582c2
LeonardoKaprio <leonardoken02@gmail.com>
2025-04-29 08:03:52 -0300

    Transaction.sql

--- Transaction.sql
+++ Transaction.sql
@@ -1,17 +1,22 @@
-create database aulaTransacao
+create database aulaTransacao 
+  
 use aulaTransacao
+  
 create table contas(
 Idconta INT primary key,
 Saldo Decimal(10,2)
 );
+
 insert into Contas(Idconta, Saldo) Values (1, 1000.00);
 insert into Contas(Idconta, Saldo) Values (2, 500.00);
 
-Begin transaction; 
+Begin transaction;  *execute esse primeiro, a partir daqui pode voltar no passado*
+
 Update contas set Saldo = Saldo - 100 Where Idconta = 1;
 Update contas set Saldo = Saldo + 200 Where Idconta = 2;
 
-Rollback;
-Commit;	
+Rollback; *Desfaz tudo e encerra*
+
+Commit;	*Confirma tudo e encerra*
 
 select*from contas
